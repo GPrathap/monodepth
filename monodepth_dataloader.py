533bb96e4d8f3c67638eb6d1d0083cce3e863050
GPrathap <ggeesara@gmail.com>
2018-09-13 12:00:24 +0300

    initial testing

--- monodepth_dataloader.py
+++ monodepth_dataloader.py
@@ -101,6 +101,7 @@ class MonodepthDataloader(object):
         path_length = string_length_tf(image_path)[0]
         file_extension = tf.substr(image_path, path_length - 3, 3)
         file_cond = tf.equal(file_extension, 'jpg')
+        print("file extension: {}".format(file_cond))
         
         image = tf.cond(file_cond, lambda: tf.image.decode_jpeg(tf.read_file(image_path))
                          , lambda: tf.image.decode_png(tf.read_file(image_path)))
