7af685e5d0349c78e8cf332315e15c90b3711193
GPrathap <ggeesara@gmail.com>
2018-09-13 12:21:46 +0300

    initial testing

--- monodepth_simple.py
+++ monodepth_simple.py
@@ -54,7 +54,7 @@ def test_simple(params):
 
     input_image = scipy.misc.imread(args.image_path, mode="RGB")
     original_height, original_width, num_channels = input_image.shape
-    input_image = scipy.misc.imresize(input_image, [args.input_height, args.input_width], interp='lanczos')
+    input_image = scipy.misc.imresize(input_image, [args.input_height, args.input_width])
     input_image = input_image.astype(np.float32) / 255
     input_images = np.stack((input_image, np.fliplr(input_image)), 0)
 
