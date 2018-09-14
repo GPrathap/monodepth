1dbe62cdc816fb3e57773b287d436e8737f48499
GPrathap <ggeesara@gmail.com>
2018-09-14 16:39:43 +0300

    initial testing

--- view_image.py
+++ view_image.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-disp_pp = np.load("/home/geesara/diparity/disparities.npy")
+disp_pp = np.load("/home/geesara/diparity/disparities_pp.npy")
 for i in range(0,len(disp_pp)):
     disp_to_img = scipy.misc.imresize(disp_pp[i].squeeze(), [256, 512])
     plt.imsave(os.path.join("/home/geesara/diparity/images", "{}_{}disp.png".format(i, "new_image")), disp_to_img, cmap='plasma')
