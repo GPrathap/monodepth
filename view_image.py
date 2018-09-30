8ff4dbbebba25548968cd51072b5d537f0aa3469
GPrathap <ggeesara@gmail.com>
2018-09-30 23:12:35 +0300

    avoiding committing *.png files

--- view_image.py
+++ view_image.py
@@ -13,4 +13,5 @@ import os
 disp_pp = np.load("/home/geesara/diparity/disparities_pp.npy")
 for i in range(0,len(disp_pp)):
     disp_to_img = scipy.misc.imresize(disp_pp[i].squeeze(), [256, 512])
-    plt.imsave(os.path.join("/home/geesara/diparity/images", "{}_{}disp.png".format(i, "new_image")), disp_to_img, cmap='plasma')
+    plt.imsave(os.path.join("/home/geesara/diparity/images", "{}_{}disp.png".format(i, "new_image"))
+               , disp_to_img, cmap='plasma')
