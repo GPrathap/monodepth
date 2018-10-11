210e0da2b19ab65a8fd881c890f40547056e543f
GPrathap <ggeesara@gmail.com>
2018-10-11 11:33:50 +0300

    adding model exporting initial testing

--- monodepth_main.py
+++ monodepth_main.py
@@ -345,8 +345,8 @@ def export_model(params):
         # it should return False in this case
           # [[ False ]] Yay, it works!
         result = np.array([y_out.squeeze()])
-        y_out = post_process_disparity(result)
-        np.save('/home/a.gabdullin/geesara/disparities_pp.npy', y_out)
+        # y_out = post_process_disparity(result)
+        np.save('/home/a.gabdullin/geesara/disparities_pp.npy', result)
     # """Test function."""
     # dataloader = MonodepthDataloader(args.data_path, args.filenames_file, params, args.dataset, args.mode)
     # left = dataloader.left_image_batch
